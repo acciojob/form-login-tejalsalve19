a07ae1b81d72a72695f9ba6264b3dae0e1ec81e7
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-27 16:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", getFormValue() {
+document.addEventListener("DOMContentLoaded", function() {
 function getFormvalue() 
 {
     //Write your code here
 	// Get the form element by its ID
 	const form = document.getElementById("form1");
 	// Get the input values from the form
-	const firstName = form.elements["fname"].value;
-	const lastName = form.elements["lame"].value;
+	const fname = form.elements["firstName"].value;
+	const lname = form.elements["lastName"].value;
 	// Display the first and last name using alert()
-	alert("First Name: " + firstName + "\nLast Name: " + lastName);
+	alert("First Name: " + fname + "\nLast Name: " + lname);
 }
 });
